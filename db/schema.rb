5b36ba148b26f5ce28848b3261fe84505512c52c
Charlie <pikec20@student.wcsc.org.uk>
2024-07-03 10:30:16 +0100

    added grades

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_091028) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_03_091853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_091028) do
     t.string "climb_type"
     t.integer "attempts"
     t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "grades", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
