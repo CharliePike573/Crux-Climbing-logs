79c8b1f7cce00b6d70a2317a91a620638a7b3ef9
Charlie <pikec20@student.wcsc.org.uk>
2024-07-04 09:30:28 +0100

    added frade category conroller

---
+++ app/controllers/grade_categories_controller.rb
@@ -0,0 +1,12 @@
+    class GradeCategoryController < ApplicationController
+        def index
+            @grade_categories = GradeCategory.all
+        end
+
+    def grades
+        @grades = Grade.where(grade_category_id: params[:id])
+        respond_to do |format|
+            format.turbo_stream
+        end
+    end
+end
